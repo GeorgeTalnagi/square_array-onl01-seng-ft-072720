27affc86f09a0fe411503c2dda9f03f7ddd1cf9f
George Talnagi <gtalnagi@yahoo.com>
2020-07-29 18:07:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |new_number|
-    new_number **2
-    new_array << new_number
+    new_array << new_number **2
   end
   return new_array
 end
